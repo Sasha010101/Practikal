a8fc91dd69b54afefc50623141c2f987f1fef46a
Sasha010101 <limon4ik201999@gmail.com>
2023-07-15 21:26:18 +0300

    update

---
+++ js/practical.js
@@ -0,0 +1,55 @@
+// const message = "JavaScript is awesome!";
+// console.log("message");
+
+// const username = "Mango";
+// console.log("Username is ", username);
+
+// const message = "JavaScript is awesome!";
+// alert("messege");
+
+// const isComing = confirm("Please confirm hotel reservation");
+// console.log("isComing");
+
+// const hotelName = prompt("Please enter desired hotel name");
+// console.log(hotelName);
+
+// const value = prompt("Please enter a number!");
+// console.log(typeof value); // "string"
+// console.log(value); // "5
+
+// const x =10;
+// const b = 596;
+// console.log( x * b);
+
+// let value =5
+// value += 10;
+// console.log(value);
+
+// const x = 5;
+// const y = 10;
+// const z = 5;
+// console.log = prompt ("x > y:", x > y);
+// console.log = prompt ("x < y:", x < y);
+
+// a > b і a < b - більше/менше
+// a >= b і a <= b - більше/менше або дорівнює
+// a == b - рівність
+// a != b - нерівність
+// a === b - строга рівність
+// a !== b - строга нерівність
+
+// const valueA = "5";
+// console.log(Number(valueA)); // 5
+// console.log(typeof Number(valueA)); // "number"
+
+// const valueB = "random string";
+// console.log(Number(valueB)); // NaN
+// console.log(typeof Number(valueB)); // "number"
+
+// console.log(Number.parseInt("5px")); // 5
+// console.log(Number.parseInt("12qwe74")); // 12
+// console.log(Number.parseInt("12.46qwe79")); // 12
+// console.log(Number.parseInt("qweqwe")); // NaN
+
+// console.log((0.17 + 0.24) .toFixed(2));
+console.log(Math(0.7));
