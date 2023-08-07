bc2dd2fcc80a4dfc79580df4cf8f9b0f23cfd2bb
Sasha010101 <limon4ik201999@gmail.com>
2023-08-07 19:44:24 +0300

    update

--- js/practical.js
+++ js/practical.js
@@ -160,14 +160,74 @@
 // console.log(calculateTotal(7));
 // console.log(calculateTotal(18));
 // console.log(calculateTotal(30));
-function calculateTotalPrice(order) {
-  let total = 0;
-  // Change code below this line
 
-  for (let total of order) {
-    total += order;
-  }
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
 
-  // Change code above this line
-  return total;
-}
+//   for (let total of order) {
+//     total += order;
+//   }
+
+//   // Change code above this line
+//   return total;
+// }
+
+// task-1
+
+
+// Спільними елементами масивів називають ті елементи, які присутні у всіх масивах.
+// Наприклад, у двох масивах [1, 3, 5] і [0, 8, 5, 3] спільними будуть числа 3 і 5, оскільки вони присутні в обох вихідних масивах. А числа 0, 1 і 8 присутні тільки в одному з масивів.
+// Напиши функцію getCommonElements(array1, array2), яка отримує два масиви довільної довжини в параметри array1 і array2, і повертає новий масив, що складається з тих елементів, які присутні в обох вихідних масивах.
+// ТЕСТИ
+// •  Оголошена функція getCommonElements(array1, array2)
+// •  Виклик getCommonElements([1, 2, 3], [2, 4]) повертає [2]
+// •  Виклик getCommonElements([1, 2, 3], [2, 1, 17, 19]) повертає [1, 2]
+// •  Виклик getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]) повертає [12, 27, 3]
+// •  Виклик getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]) повертає [10, 30, 40]
+// •  Виклик getCommonElements([1, 2, 3], [10, 20, 30]) повертає []
+// •  Виклик функції getCommonElements() з випадковими двома масивами повертає правильний масив
+// •  В циклі for використовувалися методи includes і push
+
+// function getCommonElements(array1, array2) {
+// 1.Створення пустого масиву.
+// 2.Перебрати масиву за допомогою for.
+  // 3.Умова яка порівнює елемент одного масиву з елементами іншого масиву.
+  // 4. якщо умова стверджується "True" цей елемент додається в пустий масив.
+  // в інших випадках "false".
+  // 5.функція виводить результат в пустий масив.
+  
+
+//   const result = []; // 1.
+//   for (let i = 0; i < array1.length; i++){  // 2.
+//     if(array2.includes(array1[i])){ // 3
+   
+//         result.push(array1[i]);
+    
+//   }
+// };
+// return result;
+// }
+
+// console.log(getCommonElements([1, 2, 3], [2, 4])); // [2]
+// console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19])); //[1, 2]
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27])); // [12, 27, 3]
+// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40])); // [10, 30, 40]
+// console.log(getCommonElements([1, 2, 3], [10, 20, 30])); // []
+
+// function getCommonElements(array1, array2) {
+//   const result = [];
+//   for (let i = 0; i < array1.length; i += 1) {
+//     if (array2.includes(array1[i])) {
+//       result.push(array1[i]);
+//     }
+//   }
+//   return result;
+// }
+// const hotelName = alert("Please enter desired hotel name");
+// console.log(hotelName);
+const num1 = 15;
+const num2 = 9;
+const biggerNumber = num1 > num2 ? num1 : num2;
+
+console.log(biggerNumber);
