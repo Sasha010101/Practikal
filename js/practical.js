50753d5a9f951f01fcaf005e394119d8f7c6adc8
Саша <limon4ik201999@gmail.com>
2024-03-22 15:30:20 +0200

    hwe

--- js/practical.js
+++ js/practical.js
@@ -16,3 +16,36 @@
 // const newClients = ["Peach", "Houston"];
 // const allClients = oldClients.concat(newClients);
 // console.log(allClients);
+
+
+// const books = [
+//     { title: "The Last Kingdom", author: "Bernard Cornwell" },
+//     { title: "Beside Still Waters", author: "Robert Sheckley" },
+//     { title: "The Dream of a Ridiculous Man", author: "Fyodor Dostoevsky" }
+//   ];
+  
+//   const authorToSearchFor = "Robert Sheckley";
+  
+//   for (const book of books) {
+//       if(book.author === authorToSearchFor) {
+//           console.log(book);
+//           console.log(book.title)
+//           console.log(book.rating)
+//       }
+//   }
+// function getAllPropValues(propName) {
+//     const products = [
+//       { name: "Radar", price: 1300, quantity: 4 },
+//       { name: "Scanner", price: 2700, quantity: 3 },
+//       { name: "Droid", price: 400, quantity: 7 },
+//       { name: "Grip", price: 1200, quantity: 9 },
+//     ];
+//     let propValues = [];
+//     for(let product of products){
+//       if(product === propName){
+//         return propValues.push(product[propName]);
+//       }
+//     }
+//     return propValues;
+//   }
+  
