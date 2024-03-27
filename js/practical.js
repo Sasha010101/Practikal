c67cba5a9d3e93eace8e0c1f2e1dd8ce209295b7
Саша <limon4ik201999@gmail.com>
2024-03-27 22:36:47 +0200

    practical23

--- js/practical.js
+++ js/practical.js
@@ -48,4 +48,49 @@
 //     }
 //     return propValues;
 //   }
+// function foo(...args) {
+// 	console.log(args);
+// }
+// foo("a", "b");
+
+
+// const lastWeekTemps = [14, 25, 11];
+// const currentWeekTemps = [23, 17, 17];
+// const allTemps = [...lastWeekTemps, ...currentWeekTemps];
+// console.log(allTemps);
+
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+
+// const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+// const bestScore = allScores;
+// const worstScore = allScores;
+
+
+// const objA = { 
+// 	x: 1, 
+// 	y: 2, 
+// };
+
+// const objB = { 
+// 	y: 3, 
+// };
+
+const result = {
+	x: 5,
+	...objA,
+	y: 10,
+	... objB,
+	z: 15
+}
+
+
+function greet(name) {
+	return `Welcome ${name}!`;
+  }
   
+  // Викликаємо функцію greet і виводимо результат у консоль
+  console.log(greet); // "Welcome Mango!"
+  
+
