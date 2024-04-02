ccdc5e19fcf2e1653453fb85c12274174c6a8a3e
Саша <limon4ik201999@gmail.com>
2024-04-02 20:30:42 +0300

    update

--- js/practical.js
+++ js/practical.js
@@ -10,72 +10,125 @@
 //   return acc + averagePlaytimePerGame;
 // }, 0);
 
-const calculateTotalBalance = (users) => {
-  return users.reduce((acc, user) => {
-    return acc + user.balance
-  }, 0)};
- const sumUsers = [
-   {
-     name: "Moore Hensley",
-     email: "moorehensley@indexia.com",
-     eyeColor: "blue",
-     friends: ["Sharron Pace"],
-     isActive: false,
-     balance: 2811,
-     gender: "male"
-   },
-   {
-     name: "Sharlene Bush",
-     email: "sharlenebush@tubesys.com",
-     eyeColor: "blue",
-     friends: ["Briana Decker", "Sharron Pace"],
-     isActive: true,
-     balance: 3821,
-     gender: "female"
-   },
-   {
-     name: "Ross Vazquez",
-     email: "rossvazquez@xinware.com",
-     eyeColor: "green",
-     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-     isActive: false,
-     balance: 3793,
-     gender: "male"
-   },
-   {
-     name: "Elma Head",
-     email: "elmahead@omatom.com",
-     eyeColor: "green",
-     friends: ["Goldie Gentry", "Aisha Tran"],
-     isActive: true,
-     balance: 2278,
-     gender: "female"
-   },
-   {
-     name: "Carey Barr",
-     email: "careybarr@nurali.com",
-     eyeColor: "blue",
-     friends: ["Jordan Sampson", "Eddie Strong"],
-     isActive: true,
-     balance: 3951,
-     gender: "male"
-   },
-   {
-     name: "Blackburn Dotson",
-     email: "blackburndotson@furnigeer.com",
-     eyeColor: "brown",
-     friends: ["Jacklyn Lucas", "Linda Chapman"],
-     isActive: false,
-     balance: 1498,
-     gender: "male"
-   },
-   {
-     name: "Sheree Anthony",
-     email: "shereeanthony@kog.com",
-     eyeColor: "brown",
-     friends: ["Goldie Gentry", "Briana Decker"],
-     isActive: true,
-     balance: 2764,
-     gender: "female"
-   }
- ];
+// const calculateTotalBalance = (users) => {
+//   return users.reduce((acc, user) => {
+//     return acc + user.balance
+//   }, 0)};
+//  const sumUsers = [
+//    {
+//      name: "Moore Hensley",
+//      email: "moorehensley@indexia.com",
+//      eyeColor: "blue",
+//      friends: ["Sharron Pace"],
+//      isActive: false,
+//      balance: 2811,
+//      gender: "male"
+//    },
+//    {
+//      name: "Sharlene Bush",
+//      email: "sharlenebush@tubesys.com",
+//      eyeColor: "blue",
+//      friends: ["Briana Decker", "Sharron Pace"],
+//      isActive: true,
+//      balance: 3821,
+//      gender: "female"
+//    },
+//    {
+//      name: "Ross Vazquez",
+//      email: "rossvazquez@xinware.com",
+//      eyeColor: "green",
+//      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//      isActive: false,
+//      balance: 3793,
+//      gender: "male"
+//    },
+//    {
+//      name: "Elma Head",
+//      email: "elmahead@omatom.com",
+//      eyeColor: "green",
+//      friends: ["Goldie Gentry", "Aisha Tran"],
+//      isActive: true,
+//      balance: 2278,
+//      gender: "female"
+//    },
+//    {
+//      name: "Carey Barr",
+//      email: "careybarr@nurali.com",
+//      eyeColor: "blue",
+//      friends: ["Jordan Sampson", "Eddie Strong"],
+//      isActive: true,
+//      balance: 3951,
+//      gender: "male"
+//    },
+//    {
+//      name: "Blackburn Dotson",
+//      email: "blackburndotson@furnigeer.com",
+//      eyeColor: "brown",
+//      friends: ["Jacklyn Lucas", "Linda Chapman"],
+//      isActive: false,
+//      balance: 1498,
+//      gender: "male"
+//    },
+//    {
+//      name: "Sheree Anthony",
+//      email: "shereeanthony@kog.com",
+//      eyeColor: "brown",
+//      friends: ["Goldie Gentry", "Briana Decker"],
+//      isActive: true,
+//      balance: 2764,
+//      gender: "female"
+//    }
+//  ];
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const authors = [
+//   "Tanith Lee",
+//   "Bernard Cornwell",
+//   "Robert Sheckley",
+//   "Fyodor Dostoevsky",
+// ];
+
+// const ascendingReleaseDates = releaseDates.toSorted();
+
+// const alphabeticalAuthors = authors.toSorted();
+// console.log(authors.toSorted());
+// console.log(releaseDates.toSorted());
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   {
+//     title: "The Dreams in the Witch House",
+//     author: "Howard Lovecraft",
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
+// const names = books.filter(book => book.rating > MIN_BOOK_RATING)
+//   .map(user => user.author)
+//   .toSorted((a, b) => a.localeCompare(b));
+// console.log(names);
+
+
+
+const user = {
+  username: "Victor",
+  showName() {
+		// ✅ Віктор біжить швидко, тому що він (this) намагається зловити поїзд.
+    console.log(this.username);
+  },
+};
+
+user.showName();
