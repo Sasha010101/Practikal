12183e42ef31df4907b2e8704dd3baf56d2c56e5
Sasha010101 <limon4ik201999@gmail.com>
2024-05-08 22:41:12 +0300

    hw

--- js/hw.js
+++ js/hw.js
@@ -107,7 +107,7 @@
 
 // 1. Знайдіть всіх користувачів у масиві об'єктів, які мають вік більше 18 років.
 // function searchHighAge(arr_obj) {
-//     return arr_obj.filter((arr) => arr.age > 18);
+//     return arr_obj.filter((user_obj) => user_obj.age > 18);
 // };
 // const newArr = [
 //     {name: "Masha", age: 25},
@@ -132,6 +132,7 @@
 // 3. Обчисліть загальну суму вартості усіх товарів у масиві об'єктів, де кожен об'єкт містить властивість "ціна".
 // function sumArrObjPrice(arr_obj) {
 //     return arr_obj.reduce((sum, obj) => {
+//         console.log(sum);
 //        return sum + obj.price;
 //     }, 0);
 // };
@@ -146,20 +147,82 @@
 
 // 4. Знайдіть перший об'єкт у масиві, в якому властивість "назва" має значення "продукт".
 
-function searchProduct(arr_obj) {
-    return arr_obj.find((arr) => arr.price === "product");
-};
-const newArr = [
-    {brand: "BMW", price: 30000},
-    {brand: "Audi", price:  25000},
-    {brand: "Ford", price: "product"},
-    {brand: "Kia", price: 44000 },
-];
+// function searchProduct(arr_obj) {
+//     return arr_obj.find((arr) => arr.price === "product");
+// };
+// const newArr = [
+//     {brand: "BMW", price: "product"},
+//     {brand: "Audi", price:  25000},
+//     {brand: "Ford", price: "product"},
+//     {brand: "Kia", price: 44000 },
+// ];
+// console.log(searchProduct(newArr));
 
-console.log(searchProduct(newArr));
 // 5. Створіть новий масив об'єктів, де значення кожної властивості буде збільшено на 10%.
 // 6. Отримайте список об'єктів у масиві, в яких властивість "кількість" більше 5.
 // 7. Обчисліть середнє значення властивості "оцінка" у масиві об'єктів.
+
+function getAverageScoreValue(arr_obj) {
+    return arr_obj.reduce((sum, obj) =>  sum += obj.score, 0)/arr_obj.length //сумма  = 30/3 => 3 = arr_obj.length
+};
+
+// const CustomButton:FC<ICustomButton> = ({
+//     id,
+//     title,
+//     onClickHandler,
+//     disabled,    
+//     className,
+//     activeStyles,
+// }) => {
+//     return (
+//         <button
+//             id={id}
+//             disabled={disabled}
+//             onClick={onClickHandler}
+//             className={className}
+//             styles={{isActive ? activeStyles : ""}}
+//         >
+//             {title}
+//         </button>
+//     )
+// }
+
+// props = {
+//     id: 'test',
+//     className: 'mt-10'
+// }
+
+// const CustomButton:FC<ICustomButton> = (props) => {
+//     return (
+//         <button
+//             id={props.id}
+//             disabled={props.disabled}
+//             onClick={props.onClickHandler}
+//             className={props.className}
+//         >
+//             {props.title}
+//         </button>
+//     )
+// }
+
+// let user = [
+//     { since: "biology", score: 10 },
+//     { since: "algebra", score: 9 },
+//     { since: "geometry", score: 11 },
+// ];
+// console.log(getAverageScoreValue(user));
+
 // 8. Знайдіть перший об'єкт у масиві, де властивість "категорія" містить підстроку "електроніка".
 // 9. Створіть новий масив, в якому кожний об'єкт містить властивість "прізвище" зі значенням, яке є об'єднанням властивостей "ім'я" та "прізвище".
 // 10. Отримайте список об'єктів у масиві, де властивість "доступність" має значення true.
+
+
+
+<CustomButton 
+    id='test'
+    title="Click me!"
+    onClickHandler={()=>{console.log('Hello')}}
+    disabled={false}
+    className="mt-10 text-[50px] font-bold underline text-blue-500"
+    activeStyles="shadow-md"
+/>
