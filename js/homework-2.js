81d96a27138d6742cc55c270e61df1173de9b2bb
Саша <limon4ik201999@gmail.com>
2024-06-25 20:32:57 +0200

    class

--- js/homework-2.js
+++ js/homework-2.js
@@ -1,10 +1,16 @@
 // 1. Завдання:
-//    - Дано масив об'єктів користувачів із властивостями name, age та email.
-//    - Поверніть масив імен користувачів, чий вік більше 18 і чий email містить домен example.com.
+//  - Дано масив об'єктів користувачів із властивостями name, age та email.
+//  - Поверніть масив імен користувачів, чий вік більше 18 і чий email містить домен example.com.
 // function getAdultUsers(arr_obj) {
 //   return arr_obj.filter(
-//     (user) => user.age > 18 && user.email.endsWith("@example.com")
+//     ({ age, email }) => age > 18 && email.endsWith("@example.com")
 //   );
+// }
+
+// function getAdultUsersNames(arr_obj) {
+//   return arr_obj
+//     .filter(({ age, email }) => age > 18 && email.endsWith("@example.com"))
+//     .map(({ name }) => name);
 // }
 
 // const users = [
@@ -12,17 +18,17 @@
 //   { name: "Bob", age: 17, email: "bob@other.com" },
 //   { name: "Charlie", age: 22, email: "charlie@example.com" },
 // ];
+
+// console.log(getAdultUsersNames(users));
 // console.log(getAdultUsers(users));
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 2. Завдання:
 //    - Дано масив об'єктів товарів із властивостями productName, price та category.
 //    - Поверніть масив назв товарів, ціна яких менша за 100 і які належать до категорії electronics.
-// function logNamesPriceLessAndBelongsCategory(arr_obj) {
+// function getNamesPriceLessAndBelongsCategory(arr_obj) {
 //   return arr_obj
-//     .filter(
-//       (product) => product.price < 100 && product.category === "electronics"
-//     )
-//     .map((product) => product.productName);
+//     .filter(({price,category}) => price < 100 && category === "electronics")
+//     .map(({productName}) => productName);
 // }
 // const products = [
 //   { productName: "Laptop", price: 999, category: "electronics" },
@@ -30,26 +36,29 @@
 //   { productName: "Keyboard", price: 75, category: "electronics" },
 //   { productName: "Chair", price: 150, category: "furniture" },
 // ];
-// console.log(logNamesPriceLessAndBelongsCategory(products));
+// console.log(getNamesPriceLessAndBelongsCategory(products));
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // . Завдання:
 //    - Дано масив рядків, кожен з яких є адресою електронної пошти.
 //    - Поверніть масив доменів електронних поштових скриньок, які містять у домені підрядок mail.
-// for (const iterator of object) {
-//   function getUniqEmail(arr) {
-//     //   return arr.filter((email) => email.endsWith("mail.com"));
-//     return arr.filter((email) => {
-//       const domen = email.split("@")[1];
-//       return domen.includes("mail");
-//     });
-//   }
-//   const emails = [
-//     "user1@mail.com",
-//     "user2@gmail.com",
-//     "user3@mailservice.com",
-//     "user4@yahoo.com",
-//   ];
-//   console.log(getUniqEmail(emails));
+//
+// function getUniqEmail(arr) {
+//   //   return arr.filter((email) => email.endsWith("mail.com"));
+//   return arr.filter((email) => {
+//     const domen = email.split("@")[1];
+//     return domen.includes("mail");
+//   });
+// }
+
+// // return arr.filter((email) => email.split("@")[1].includes("mail"));
+
+// const emails = [
+//   "user1@mail.com",
+//   "user2@gmail.com",
+//   "user3@mailservice.com",
+//   "user4@yahoo.com",
+// ];
+// console.log(getUniqEmail(emails));
 // };
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 4. Завдання:
@@ -57,7 +66,7 @@
 //    - Поверніть масив об'єктів студентів, у яких спеціальність computer science, а оцінка більше 85.
 // function geScoreStudentsAndUniqMajor(arr_obj) {
 //   return arr_obj.filter(
-//     (student) => student.major === "computer science" && student.score > 85
+//     ({major, score}) => major === "computer science" && score > 85
 //   );
 // }
 
@@ -74,9 +83,9 @@
 //    - Дано масив чисел.
 //    - Поверніть масив об'єктів із двома властивостями: number та isEven, де isEven дорівнює true, якщо число парне, і false, якщо число непарне.
 // function getArrObjWithTwoKeys(arr_numbers) {
-//   return numbers.map((number) => ({
+//   return arr_numbers.map((number) => ({
 //     number: number,
-//     isEven: number % 2 === 0,
+//     isEven: number % 2 === 0,//!(number % 2)
 //   }));
 // }
 // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -87,7 +96,7 @@
 //    - Поверніть масив назв книг, які були написані після 2000 року і автори яких починаються на букву "J".
 // function getArrNewersBooksWithLetterJ(arr_obj_books) {
 //   return arr_obj_books.filter(
-//     (book) => book.year > 2000 && book.author.startsWith("J")
+//     ({year, author}) => year > 2000 && author.startsWith("J")
 //   );
 // }
 // const books = [
@@ -101,15 +110,119 @@
 // 7. Завдання:
 //    - Дано масив рядків, кожен з яких є фразою.
 //    - Поверніть масив фраз, які містять слово JavaScript, і зробіть їх усі великими літерами.
-function getarrFrazWithWordJavaScrips(arr) {
-  return arr
-    .filter((phrase) => phrase.includes("JavaScript"))
-    .map((phrase) => phrase.toUpperCase());
+// function getarrFrazWithWordJavaScrips(arr) {
+//   return arr
+//     .filter((phrase) => phrase.includes("JavaScript"))
+//     .map((phrase) => phrase.toUpperCase());
+// }
+// const phrases = [
+//   "I love JavaScript",
+//   "JavaScript is fun",
+//   "I enjoy coding",
+//   "Programming in JavaScript",
+// ];
+// console.log(getarrFrazWithWordJavaScrips(phrases));
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 8. Завдання:
+//    - Дано масив об'єктів продуктів із властивостями productName, price та available.
+//    - Поверніть масив назв продуктів, які доступні (`available: true`) і ціна яких більша за 50.
+
+// function getFilteredproductNames(arr_obj) {
+//   return arr_obj
+//     .filter(({ available, price }) => available && price > 50)
+//     .map(({ productName }) => productName);
+// }
+
+// function maybeFaster(arr_obj) {
+//   let result = [];
+//   arr_obj.forEach(({ productName, price, available }) => {
+//     available && price > 50 ? result.push(productName) : "";
+//   });
+//   return result;
+// }
+
+// const items = [
+//   { productName: "Phone", price: 600, available: true },
+//   { productName: "Tablet", price: 300, available: false },
+//   { productName: "Laptop", price: 1000, available: true },
+//   { productName: "Headphones", price: 30, available: true },
+//   { productName: "Phone1", price: 600, available: true },
+//   { productName: "Tablet1", price: 300, available: false },
+//   { productName: "Laptop1", price: 1020, available: true },
+//   { productName: "Headphones1", price: 30, available: true },
+//   { productName: "Phone2", price: 600, available: true },
+//   { productName: "Tablet2", price: 300, available: false },
+//   { productName: "Laptop2", price: 1040, available: true },
+//   { productName: "Headphones2", price: 40, available: true },
+//   { productName: "Phone3", price: 600, available: true },
+//   { productName: "Tablet3", price: 300, available: false },
+//   { productName: "Laptop3", price: 1000, available: false },
+//   { productName: "Headphones3", price: 20, available: true },
+//   { productName: "Phone4", price: 600, available: true },
+//   { productName: "Tablet4", price: 300, available: false },
+//   { productName: "Laptop4", price: 1020, available: false },
+//   { productName: "Headphones4", price: 30, available: true },
+//   { productName: "Phone5", price: 600, available: true },
+//   { productName: "Tablet5", price: 300, available: false },
+//   { productName: "Laptop5", price: 1000, available: true },
+//   { productName: "Headphones5", price: 50, available: true },
+//   { productName: "Phone6", price: 600, available: true },
+//   { productName: "Tablet6", price: 300, available: false },
+//   { productName: "Laptop6", price: 1010, available: false },
+//   { productName: "Headphones6", price: 30, available: true },
+// ];
+// console.time("first");
+// console.log(getFilteredproductNames(items));
+// console.timeEnd("first");
+
+// console.time("second");
+// console.log(maybeFaster(items));
+// console.timeEnd("second");
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 9. Завдання:
+//    - Дано масив об'єктів користувачів із властивостями name, subscriptionType (`basic`, premium, `gold`).
+//    - Поверніть масив імен користувачів, які мають підписку типу premium або gold.
+function getVipSubscriptionUsers(users_arr) {
+  return users_arr
+    .filter(
+      ({ subscriptionType }) =>
+        subscriptionType === "premium" || subscriptionType === "gold"
+    )
+    .map(({ name }) => name);
 }
-const phrases = [
-  "I love JavaScript",
-  "JavaScript is fun",
-  "I enjoy coding",
-  "Programming in JavaScript",
+const users = [
+  { name: "Alice", subscriptionType: "basic" },
+  { name: "Bob", subscriptionType: "premium" },
+  { name: "Charlie", subscriptionType: "gold" },
+  { name: "Dave", subscriptionType: "basic" },
 ];
-console.log(getarrFrazWithWordJavaScrips(phrases));
+console.log(getVipSubscriptionUsers(users));
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 12. Завдання:
+//     - Дано масив об'єктів employees із властивостями name, department, salary.
+//     - Поверніть масив об'єктів із властивостями name та adjustedSalary,{name:'g', adjustedSalary:676}
+// де adjustedSalary дорівнює salary, !якщо департамент не HR!, і salary помножене на 1.1, !якщо департамент HR!.
+function getArrWithKeys(employees_arr) {
+  return employees_arr.map(({ name, department, salary }) => {
+    if (department !== "HR") {
+      return {
+        name, //name:name->bame
+        adjustSalary: salary,
+      };
+    } else if (department === "HR") {
+      return {
+        name,
+        adjustSalary: salary * 1.1,
+      };
+    }
+  });
+}
+const employees = [
+  { name: "Ivy", department: "HR", salary: 50000 },
+  { name: "Jack", department: "Engineering", salary: 70000 },
+  { name: "Kelly", department: "HR", salary: 55000 },
+  { name: "Leo", department: "Marketing", salary: 60000 },
+];
+console.log(getArrWithKeys(employees));
